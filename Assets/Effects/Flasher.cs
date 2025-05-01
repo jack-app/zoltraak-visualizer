04d0d7868c6ebc3a6385d2be1b0252a69b5e3217
mikii-f <mh.umafate@outlook.jp>
2025-05-01 20:57:56 +0900

    微修正

--- Assets/Effects/Flasher.cs
+++ Assets/Effects/Flasher.cs
@@ -6,10 +6,14 @@ public class Flasher : MonoBehaviour
 {
     [SerializeField] private PostProcessVolume postProcessVolume;
     private Bloom bloom;
-    private const int defaultIntensity = 10;
-    private const int maxIntensity = 20;
-    private const float defaultThreshold = 0.9f;
-    private const float minThreshold = 0.3f;
+    public int defaultIntensity = 10;
+    public int circleIntensity = 30;
+    public int beamIntensity = 15;
+    public int finishIntensity = 25;
+    public float defaultThreshold = 0.9f;
+    public float circleThreshold = 0.2f;
+    public float beamThreshold = 0.7f;
+    public float finishThreshold = 0.3f;
 
     void Start()
     {
@@ -20,40 +24,39 @@ public class Flasher : MonoBehaviour
     public IEnumerator Flash1()
     {
         //0.25秒で発光最大化→0.25秒で収束
-        //魔法陣だけでは明るさが足りない気がしたため最後の係数で微調整
-        while (bloom.intensity < maxIntensity)
+        while (bloom.intensity < circleIntensity)
         {
-            bloom.intensity.value += (maxIntensity - defaultIntensity) * Time.deltaTime * 4 * 1.25f;
-            bloom.threshold.value -= (defaultThreshold - minThreshold) * Time.deltaTime * 4 * 0.75f;
+            bloom.intensity.value += (circleIntensity - defaultIntensity) * Time.deltaTime * 4;
+            bloom.threshold.value -= (defaultThreshold - circleThreshold) * Time.deltaTime * 4;
             yield return null;
         }
-        bloom.intensity.value = maxIntensity * 1.25f;
-        bloom.threshold.value = minThreshold * 0.75f;
-        while (bloom.intensity > defaultIntensity)
+        bloom.intensity.value = circleIntensity;
+        bloom.threshold.value = circleThreshold;
+        while (bloom.intensity > beamIntensity)
         {
-            bloom.intensity.value -= (maxIntensity - defaultIntensity) * Time.deltaTime * 4 * 1.25f;
-            bloom.threshold.value += (defaultThreshold - minThreshold) * Time.deltaTime * 4 * 0.75f;
+            bloom.intensity.value -= (circleIntensity - beamIntensity) * Time.deltaTime * 4;
+            bloom.threshold.value += (beamThreshold - circleThreshold) * Time.deltaTime * 4;
             yield return null;
         }
-        bloom.intensity.value = defaultIntensity;
-        bloom.threshold.value = defaultThreshold;
+        bloom.intensity.value = beamIntensity;
+        bloom.threshold.value = beamThreshold;
     }
     //スクリプトから発光を制御(発射終了) 
     public IEnumerator Flash2()
     {
         //0.5秒で発光最大化→1秒で収束
-        while (bloom.intensity < maxIntensity)
+        while (bloom.intensity < finishIntensity)
         {
-            bloom.intensity.value += (maxIntensity - defaultIntensity) * Time.deltaTime * 2;
-            bloom.threshold.value -= (defaultThreshold - minThreshold) * Time.deltaTime * 2;
+            bloom.intensity.value += (finishIntensity - beamIntensity) * Time.deltaTime * 2;
+            bloom.threshold.value -= (beamThreshold - finishThreshold) * Time.deltaTime * 2;
             yield return null;
         }
-        bloom.intensity.value = maxIntensity;
-        bloom.threshold.value = minThreshold;
+        bloom.intensity.value = finishIntensity;
+        bloom.threshold.value = finishThreshold;
         while (bloom.intensity > defaultIntensity)
         {
-            bloom.intensity.value -= (maxIntensity - defaultIntensity) * Time.deltaTime;
-            bloom.threshold.value += (defaultThreshold - minThreshold) * Time.deltaTime;
+            bloom.intensity.value -= (finishIntensity - defaultIntensity) * Time.deltaTime;
+            bloom.threshold.value += (defaultThreshold - finishThreshold) * Time.deltaTime;
             yield return null;
         }
         bloom.intensity.value = defaultIntensity;
