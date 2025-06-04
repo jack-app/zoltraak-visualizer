752c156f47876ac2fdd4fb6c9693b74f3c7ec2dc
KateSawada <nuksawada@gmail.com>
2025-06-04 13:46:20 +0900

    コントローラーから魔法発動できるようにした

--- Assets/Scripts/Managers/GamepadManager.cs
+++ Assets/Scripts/Managers/GamepadManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
 
 public class GamepadManager : MonoBehaviour
 {
@@ -19,8 +22,10 @@ public class GamepadManager : MonoBehaviour
     private Dictionary<int, bool> isButtonPressKept = new Dictionary<int, bool> {};
 
     private GameObject ScreenShotManager;
+    private GameObject MemoryMapFileHandler;
 
     private ScreenShotManager screenShotManagerScript;
+    private MemoryMapFileManager memoryMapFileManagerScript;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +43,14 @@ public class GamepadManager : MonoBehaviour
             screenShotManagerScript = ScreenShotManager.GetComponent<ScreenShotManager>();
         }
         Debug.Log("GamepadManager started");
+
+        MemoryMapFileHandler = GameObject.Find("MemoryMapFileHandler");
+        if (MemoryMapFileHandler == null)
+        {
+            Debug.LogError("MemoryMapFileHandler not found in the scene.");
+        } else {
+            memoryMapFileManagerScript = MemoryMapFileHandler.GetComponent<MemoryMapFileManager>();
+        }
     }
 
     // Update is called once per frame
@@ -73,6 +86,36 @@ public class GamepadManager : MonoBehaviour
             } else if (pressedButton == 1)
             {
                 StartCoroutine(screenShotManagerScript.UploadAllScreenshotsAsync());
+            }
+            else {  // spells
+                try
+                {
+                    using (var mmf = MemoryMappedFile.CreateFromFile(memoryMapFileManagerScript.spellsMmapPath, FileMode.Open, null))
+                    {
+                        using (var accessor = mmf.CreateViewAccessor(0, 2, MemoryMappedFileAccess.ReadWrite))
+                        {
+                            accessor.Write(0, (byte)(pressedButton - 1));  // button 2 -> Zoltraak (id 1)
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"mmap への書き込みに失敗: {ex.Message}");
+                }
+                try
+                {
+                    using (var mmf = MemoryMappedFile.CreateFromFile(memoryMapFileManagerScript.spellDetectMmapPath, FileMode.Open, null))
+                    {
+                        using (var accessor = mmf.CreateViewAccessor(0, 1, MemoryMappedFileAccess.ReadWrite))
+                        {
+                            accessor.Write(0, (byte)1);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError($"mmap への書き込みに失敗: {ex.Message}");
+                }
             }
         }
     }
